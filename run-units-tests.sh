aa0c0265b7f991e9885682b048660110ca91e1a7
Marc NGUYEN <nguyen_marc@live.fr>
2019-06-09 02:00:44 +0200

    Unit Tests: Delete

--- run-units-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TARGET=$(basename "$(pwd)")
-make remove -j4 && make tests -j4
-echo "--Files that will be runned--"
-find bin/ -type f ! -name "${TARGET}_server" ! -name "${TARGET}_client"
-echo "--End list--"
-find bin/ -type f ! -name "${TARGET}_server" ! -name "${TARGET}_client" -exec {} \;
